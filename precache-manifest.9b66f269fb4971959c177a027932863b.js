6f7ded0ee670cc704f2f05400cfa4388e5f47ae3
kharchenkok <k.v.kharchenko@ukr.net>
2020-09-07 01:22:20 +0300

    Updates

---
+++ precache-manifest.9b66f269fb4971959c177a027932863b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b29775444d5fc1b17690745a1f2c3f5",
+    "url": "/goit-react-hw-02-feedback/index.html"
+  },
+  {
+    "revision": "dba4a8c95676b9ae60ae",
+    "url": "/goit-react-hw-02-feedback/static/css/main.f187cc97.chunk.css"
+  },
+  {
+    "revision": "ee52df3866615962af96",
+    "url": "/goit-react-hw-02-feedback/static/js/2.2eaa0808.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-02-feedback/static/js/2.2eaa0808.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dba4a8c95676b9ae60ae",
+    "url": "/goit-react-hw-02-feedback/static/js/main.1c4c4ca8.chunk.js"
+  },
+  {
+    "revision": "34df20d6ccb90c3d8229",
+    "url": "/goit-react-hw-02-feedback/static/js/runtime-main.8dfaa35e.js"
+  }
+]);
